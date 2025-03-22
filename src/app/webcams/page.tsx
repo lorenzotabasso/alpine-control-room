47ea36d2c2a4255a494fcda54d59eab21f925e32
Lorenzo Tabasso <lorenzo.tabasso96@gmail.com>
2025-03-22 23:04:09 +0100

    Removed modal

--- src/app/webcams/page.tsx
+++ src/app/webcams/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import { webcams } from "@/lib/data";
 import { ContentType, WebcamProps } from "@/lib/definitions";
 import Image from "next/image";
@@ -29,52 +28,24 @@ function openNewTabWithSelectedWebcam(url: string | undefined): void {
 }
 
 export default function Page() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedImage, setSelectedImage] = useState<string | null>(null);
-
-  const openModal = (imageSrc: string) => {
-    setSelectedImage(imageSrc);
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setSelectedImage(null);
-    setIsModalOpen(false);
-  };
-
   const allWebcams = webcams.map((webcam) => {
     return (
       <div
         className="border border-gray-400 rounded-xl overflow-hidden"
         key={webcam.resort + "-" + webcam.label}
       >
-        <div className="group cursor-pointer relative">
-          <Image
-            src={checkWebcamSource(webcam)}
-            alt={webcam.label}
-            width={373}
-            height={210}
-          />
-          <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
-            <button
-              className="bg-white px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors"
-              onClick={() => openModal(checkWebcamSource(webcam))} // Open modal on button click
-            >
-              <Image
-                src="./fullscreen.svg"
-                alt="Open webcam in fullscreen"
-                width={24}
-                height={24}
-              />
-            </button>
-          </div>
-        </div>
+        <Image
+          src={checkWebcamSource(webcam)}
+          alt={webcam.label}
+          width={373}
+          height={210}
+        />
         <div className="p-2">
           <div className="flex justify-between items-center">
             <h3 className="text-lg font-bold">{webcam.resort}</h3>
             {webcam.contentType === ContentType.IFRAME && (
               <div className="flex justify-between items-center text-red-600">
-                <h5 className="text-sm font-normal mr-2">Live</h5>
+                <h5 className="text-sm font-normal mr-1">Live</h5>
                 <Image
                   src="./stream.svg"
                   alt="This webcam is live"
@@ -115,28 +86,6 @@ export default function Page() {
 
   return (
     <div>
-      {/* Modal */}
-      {isModalOpen && selectedImage && (
-        <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
-          <div className="relative">
-            <button
-              className="absolute top-2 right-2 bg-white text-black rounded-full p-2"
-              onClick={closeModal}
-            >
-              ✕
-            </button>
-            <Image
-              src={selectedImage}
-              alt="Selected Webcam"
-              width={800}
-              height={450}
-              className="rounded-lg"
-            />
-          </div>
-        </div>
-      )}
-
-      {/* Webcam Cards */}
       <div className="m-4 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center items-center">
         {allWebcams}
       </div>
